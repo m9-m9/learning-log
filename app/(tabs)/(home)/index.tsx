fa374bc84bb69741f282d5a51249088d08487bc8
정민규 <jmg2165@naver.com>
2025-08-29 18:21:03 +0900

    feat : 제로초 강의 업로드

---
+++ app/(tabs)/(home)/index.tsx
@@ -0,0 +1,55 @@
+import { Text, View, TouchableOpacity } from 'react-native';
+import { usePathname, useRouter } from 'expo-router';
+
+export default function Index() {
+	const router = useRouter();
+	const pathname = usePathname();
+
+	console.log('pathname', pathname);
+
+	return (
+		<View
+			style={{
+				flex: 1,
+				justifyContent: 'center',
+				alignItems: 'center',
+			}}
+		>
+			<View>
+				<TouchableOpacity onPress={() => router.navigate(`/`)}>
+					<Text style={{ color: pathname === '/' ? 'red' : 'black' }}>
+						For you
+					</Text>
+				</TouchableOpacity>
+			</View>
+			<View>
+				<TouchableOpacity onPress={() => router.navigate(`/following`)}>
+					<Text style={{ color: pathname === '/' ? 'black' : 'red' }}>
+						Following
+					</Text>
+				</TouchableOpacity>
+			</View>
+			<View>
+				<TouchableOpacity
+					onPress={() => router.push(`/@zerocho/post/1`)}
+				>
+					<Text>게시글1</Text>
+				</TouchableOpacity>
+			</View>
+			<View>
+				<TouchableOpacity
+					onPress={() => router.push(`/@zerocho/post/2`)}
+				>
+					<Text>게시글2</Text>
+				</TouchableOpacity>
+			</View>
+			<View>
+				<TouchableOpacity
+					onPress={() => router.push(`/@zerocho/post/3`)}
+				>
+					<Text>게시글3</Text>
+				</TouchableOpacity>
+			</View>
+		</View>
+	);
+}
---
+++ app/modal.tsx
@@ -0,0 +1,16 @@
+import { View, Text, Pressable } from 'react-native';
+import { useRouter } from 'expo-router';
+
+export default function Modal() {
+	const router = useRouter();
+	return (
+		<View
+			style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+		>
+			<Text> I'm a modal</Text>
+			<Pressable onPress={() => router.back()}>
+				<Text>Close</Text>
+			</Pressable>
+		</View>
+	);
+}
