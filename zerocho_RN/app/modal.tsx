662af096e69720b9295114a8bb0f90a75462bbc3
정민규 <jmg2165@naver.com>
2025-09-03 17:30:58 +0900

    chore : 아이콘 변경

--- zerocho_RN/app/modal.tsx
+++ zerocho_RN/app/modal.tsx
@@ -35,7 +35,7 @@ export function ListFooter({
 		<View style={styles.listFooter}>
 			<View style={styles.listFooterAvatar}>
 				<Image
-					source={require('../assets/images/avatar.png')}
+					source={require('../assets/images/logo.png')}
 					style={styles.avatarSmall}
 				/>
 			</View>
